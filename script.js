228ef8a4f9e0e24f72262c5ce6ec713cb48e9ca8
Scott Fennell <slfennell01@gmail.com>
2024-06-21 22:08:31 -0400

    Implement transitions and hover effects for bars
    Add: Re-arranged code

--- script.js
+++ script.js
@@ -2,34 +2,47 @@ function main(){
     let dataPoints = [100,420,230,560,925]; //data points
     let width = 500,
     margin = 1,
-    barHeight = 20,
-    height = (barHeight+margin)*dataPoints.length; // <^ svg container
+    barHeight = 20;
 
-    var svg = d3.select('body') //create svg
-        .append('svg')
-        .attr('width', width)
-        .attr('height', height);
 
-    var scale = d3.scaleLinear() // scale for bar dimensions
-        .domain([0, d3.max(dataPoints)])
-        .range([50, width]);
+    var scale = d3.scaleLinear()
+                    .domain([d3.min(dataPoints), d3.max(dataPoints)])
+                    .range([50, width]);
+    var svg = d3.select('body').append('svg')
+                    .attr('width', width)
+                    .attr('height', barHeight * dataPoints.length);
+    var group = svg.selectAll('g')
+                    .data(dataPoints)
+                    .enter()
+                    .append('g')
+                    .attr('transform', function(d,i){
+                        return 'translate(0,' + i * barHeight + ')';
+                    });
+    group.append('rect')
+                    .attr('width', function(d){
+                        return scale(d);
+                    })
+                    .attr('height', barHeight - margin)
+                    .transition()
+                    .duration(1000);
+    group.append('text')
+                    .attr('x', function(d){
+                        return (scale(d));
+                    })
+                    .attr('y', barHeight / 2)
+                    .attr('dy', '.35em')
+                    .text(function(d){return d;});
 
-    var bar = svg.selectAll('g') // g elements within the svg 
-        .data(dataPoints)
-        .enter()
-        .append('g')
-        .attr('transform', function(d,i){return `translate(0,${i*(barHeight+margin)})`;});
+    group
+        .on('mouseover', function(){
+            d3.select(this).select('rect').style('fill', 'firebrick');
+        })
+        .on('mouseout', function(){
+            d3.select(this).select('rect').style('fill', 'green');
+        })
 
-    bar.append('rect') //group for rects (bars)
-        .attr('width',0) //start ->
-        .attr('height', barHeight)
-        .transition() //add transition
-        .duration(800)
-        .attr('width', function(d){scale(d);}) //end !
+                    
+    
 
-    bar.append('text')
-        .attr('x', function(d){return (scale(d));})
-        .attr('y', barHeight/2)
-        .attr('dy', '0.35em')
-        .attr(function(d){return d;});
-} 
+    
+}
